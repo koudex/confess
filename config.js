57d8d546c9dbd22242205bc812e32269d7981c60
koudex <zishin.dev@gmail.com>
2026-02-12 13:01:15 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 const config = {
     headerText: "will you be my valentine?",
-    yesHeaderText: "yayy!",
+    yesHeaderText: "yayy! i know you'd say yes at the end",
     
     gifURL: "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExbW5lenZyZHI5OXM2eW95b3pmMG40cWVrMDhtNjVuM3A4dGNxa2g2dSZlcD12MV9pbnRlcm5naWZfYnlfaWQmY3Q9cw/VM1fcpu2bKs1e2Kdbj/giphy.gif",
     yesGifURL: "https://media.tenor.com/Rh2GZjVhteYAAAAi/tonton-friends-tonton.gif",
     
-    musicURL: "https://files.catbox.moe/ohqbir.mp3",
+    musicURL: "https://files.catbox.moe/k2o0lq.mp3",
     musicVolume: 0.3,
     
     messages: [
     "sure na ba yan?",  
     "aminin mo na kasi",  
-    "!yes",  
-    "di mo'ko gusto?", 
+    "yes",  
+    "di mo ba'ko gusto?", 
     "okay ka lang?", 
     "gusto kita",  
     "final answer?",  
